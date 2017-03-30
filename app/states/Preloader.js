d9a55ffc869cdc84c59b993a3ec696f9c18fade2
Brandon Conway <brandoncowboy@gmail.com>
2017-03-29 19:09:47 -0700

    Fix preload loading bar

--- app/states/Preloader.js
+++ app/states/Preloader.js
@@ -5,22 +5,29 @@ import {Main} from "./Main.js"
 class Preloader extends Phaser.State  {
 
   	preload() {
+        this.preloadBar = this.add.sprite(this.game.width/2-100,
+            this.game.height/2, 'preloaderBar');
+        this.preloadBar.scale.setTo(2);
 
+	    this.text = this.add.text(this.game.width/2, this.game.height/2-100,
+            "Loading...", this.game.headerStyle);
+        this.text.anchor.setTo(0.5);
+	    this.load.setPreloadSprite(this.preloadBar);
+
+        // Buttons
         this.load.image('playButton', 'assets/images/play_button.png');
         this.load.image('pauseButton', 'assets/images/pause_button.png');
         this.load.image('fullScreenButton', 'assets/images/fullscreen_button.png');
         this.load.image('reloadButton', 'assets/images/reload_button.png');
         this.load.image('shootButton', 'assets/images/shoot_button.png');
-
-        //this.load.image('player', 'assets/images/identifier.png');
-        //this.load.image('player', 'assets/images/red_fade_player.png');
+        // Game objects
         this.load.image('player', 'assets/images/blues_player2.png');
         this.load.image('house', 'assets/images/house.png');
         this.load.image('portal', 'assets/images/portal.png');
         this.load.image('ground', 'assets/images/rect.png');
         this.load.image('boar', 'assets/images/boar.png');
         this.load.image('blast', 'assets/images/ringblast.png');
-
+        // Audio
         this.load.audio('mainMusic', ['assets/audio/main.m4a']);
         this.load.audio('goalMusic', ['assets/audio/goal.m4a']);
         this.load.audio('playerOuchSound', ['assets/audio/new_ouch_sound.m4a']);
@@ -28,11 +35,6 @@ class Preloader extends Phaser.State  {
         this.load.audio('blastSound', ['assets/audio/blast2.m4a']);
         this.load.audio('jumpSound', ['assets/audio/jump_sound.m4a']);
         this.load.audio('teleportSound', ['assets/audio/teleport.m4a']);
-
-    	//this.background = this.add.sprite(0, 0, 'preloaderBackground');
-	    this.preloadBar = this.add.sprite(100, 100, 'preloaderBar');
-	    this.text = this.add.text(100, 100, "Loading...", this.headerStyle);
-	    this.load.setPreloadSprite(this.preloadBar);
     }
 
     create() {
@@ -42,10 +44,8 @@ class Preloader extends Phaser.State  {
     update() {
         this.state.add("MainMenu", MainMenu);
         this.state.start('MainMenu');
-        //this.state.add("Main", Main);
-        //this.state.start('Main');
     }
-
+    
 }
 
 export { Preloader }
